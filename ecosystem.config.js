e7d49708df688feed3f5f85378f92ca51615055e
ravipazl <ravimech476@gmail.com>
2025-07-17 03:40:45 +0530

    New Code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,18 @@ module.exports = {
   apps: [
     {
       name: 'automobile-safety-api',
-      script: 'uvicorn',
+      script: '/home/test/testapp/venv/bin/uvicorn',
       args: 'main:app --host 0.0.0.0 --port 8000',
-      cwd: 'D:/PROJECTS/testapp',
+      cwd: '/home/test/testapp',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G'
+      max_memory_restart: '1G',
+      env: {
+        PYTHONPATH: '/home/test/testapp',
+        PATH: '/home/test/testapp/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+      }
     }
   ]
 };
